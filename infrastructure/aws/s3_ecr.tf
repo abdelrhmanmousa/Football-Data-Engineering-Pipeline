02a9d2b909fd60fe3e36b0f6e727f4e97fe4ef48
abdelrhmanmousa <abdelrhmanmosa038@gmail.com>
2025-12-27 03:42:16 +0200

    added caller identity

--- infrastructure/aws/s3_ecr.tf
+++ infrastructure/aws/s3_ecr.tf
@@ -4,14 +4,17 @@ resource "aws_s3_bucket" "data_lake" {
   bucket_prefix = "${var.project_name}-lake-"
   force_destroy = true # Allows deleting bucket even if it has files (for learning)
 }
+data "aws_caller_identity" "current" {}
 
 # 2. THE GARAGE (ECR)
 resource "aws_ecr_repository" "ingestion_repo" {
-  name = "${var.project_name}-ingestion"
-  force_delete = true
+  name                 = "${var.project_name}-ingestion"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "analytics_repo" {
-  name = "${var.project_name}-analytics"
-  force_delete = true
+  name                 = "${var.project_name}-analytics"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
