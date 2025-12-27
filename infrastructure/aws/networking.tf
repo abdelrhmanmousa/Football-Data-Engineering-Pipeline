82722ad4bb7c386770b3f44c7644667f7d46fa24
abdelrhmanmousa <abdelrhmanmosa038@gmail.com>
2025-12-27 02:33:12 +0200

    added subnet defualt

--- infrastructure/aws/networking.tf
+++ infrastructure/aws/networking.tf
@@ -1,25 +1,28 @@
-# Get default VPC
-data "aws_vpc" "default" {
-  default = true
+resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "az1" {
+  availability_zone = "${var.aws_region}a"
+}
+resource "aws_default_subnet" "az2" {
+  availability_zone = "${var.aws_region}b"
 }
 
-# Get default Subnets
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# Security Group to allow outgoing internet access (to download pip packages)
 resource "aws_security_group" "ecs_sg" {
-  name   = "${var.project_name}-sg"
-  vpc_id = data.aws_vpc.default.id
+  name   = "${var.project_name}-ecs-sg"
+  vpc_id = aws_default_vpc.default.id
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# This ensures you only use the subnets you explicitly defined in code
+data "aws_subnets" "default" {
+  filter {
+    name   = "subnet-id"
+    values = [aws_default_subnet.az1.id, aws_default_subnet.az2.id]
   }
 }
